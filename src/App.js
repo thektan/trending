ffc03f07117e2ca0675babf825ed419bd7d3ca02
Kevin Tan <kevin.tan@liferay.com>
2017-06-29 10:58:05 -0700

    Formatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import DesignerNewsFeed from './components/DesignerNewsFeed'
+import DesignerNewsFeed from './components/DesignerNewsFeed';
 import HackerNewsFeed from './components/HackerNewsFeed';
 import './App.css';
 
