4e685c4b103a9f91cc97f5f660b128799adba2ba
Kevin Tan <kevin.tan@liferay.com>
2017-06-30 15:21:57 -0700

    jsdoc

--- src/utils/designer-news-api.js
+++ src/utils/designer-news-api.js
@@ -4,13 +4,17 @@ const BASE_URL = 'https://www.designernews.co/api/v2/';
 const BASE_SITE_URL = 'https://www.designernews.co/';
 
 /**
- * Gets the url to the feed site.
+ * Gets the url to the feed site in the format /stories/{id}.
  * @param {number} id The id of the feed item.
  * @return {string} The url to the main feed site.
  */
 const getSiteStoriesURL = (id) =>
 	`${BASE_SITE_URL}stories/${id}`;
 
+/**
+ * Gets the top designer news stories.
+ * @return {object} The JSON data.
+ */
 const getStories = () =>
 	fetch(`${BASE_URL}stories`)
 		.then((response) => response.json());
