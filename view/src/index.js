ee66c42da9a9822578b5d3373e6a154f210b5c0c
Kevin Tan <kevin.tan@liferay.com>
2017-09-22 18:41:48 -0700

    Add raven for sentry and also ran yarn upgrade

--- view/src/index.js
+++ view/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Raven from 'raven';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './css/index.css';
+
+Raven.config('https://8a05d76ce781435e89d659fda1f59ac0@sentry.io/221103').install();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
